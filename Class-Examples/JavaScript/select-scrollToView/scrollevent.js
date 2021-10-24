8eac451c1e9828bf5971f9cde4a8d5cefa6d2b4a
ebeshero <ebbondar@gmail.com>
2021-10-24 00:30:31 -0400

    JS for scrollIntoView from datalist

--- Class-Examples/JavaScript/select-scrollToView/scrollevent.js
+++ Class-Examples/JavaScript/select-scrollToView/scrollevent.js
@@ -1,9 +1,8 @@
-/* See Stack Overflow: 
+/* 2021-10-24 ebb: JavaScript to respond to an item selected from HTML datalist
+ * Based on second response to Stack Overflow "Perform action when clicking HTML5 datalist option"
  * https://stackoverflow.com/questions/30022728/perform-action-when-clicking-html5-datalist-option */
 
 window.addEventListener('DOMContentLoaded',pageLoadUp,false);
-
-
 
 function pageLoadUp() {
    var button = document.getElementById("scrollToTop")
@@ -11,39 +10,29 @@ function pageLoadUp() {
    var inputs = document.getElementsByTagName('input')
    for (var i = 0, length = inputs.length; i < length; i++)
    {
+      /* ebb: Listen for input event */
    inputs[i].addEventListener('input', onInput);  
    }
    
-}
-
 function onInput(e) {
     var input = e.target;
     var val = input.value;
     var list = input.getAttribute('list');
     var options = document.getElementById(list).childNodes;
-     for (var i = 0, length = options.length; i < length; i++) {
-      var value = options[i].value;
+     for (var i = 0, length = options.length; i < length; i++) 
+     { var value = options[i].value;
       if(value === val)
       {
-      // An item was selected from the list!
-      alert('item selected: ' + val);
-      var matchid = options[i].id.split('_')[1]
+        // An item was selected from the list!
+        alert('item selected: ' + val);
+        var matchid = options[i].id.split('_')[1]
         console.log('matchid is', matchid );
         var matchTable = document.getElementById(matchid)
         matchTable.scrollIntoView(true);
-      break;
+        break; }
         }
-        }
-
+    }
 }
-
-/*function navigate() {
-/\*var matchid = this.id.split('_')[1]*\/
-alert('What is my matchid?', matchid)
-console.log('What is my matchid?', matchid)
-var matchTable = document.getElementById(matchid)
-    matchTable.scrollIntoView(true);
-}*/
 
 
 function scrollToTop(){
